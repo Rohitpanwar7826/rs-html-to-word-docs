09cb794ad329016dbe083a59acad091a2027440f
Rohit Singh <rohit.panwar@gemsessence.com>
2024-05-12 17:58:29 +0530

    added main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+import { convertHtmlPageToWordJavascript } from "./javascript_index.js";
+import { convertHtmlPageToWordNode } from "./node_index.js";
+
+export {
+  convertHtmlPageToWordJavascript,
+  convertHtmlPageToWordNode,
+}
